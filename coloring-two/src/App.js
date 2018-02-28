678e4d6d66300761dfd02f066417f04d715c3e2e
Travis Meamber <tmeamber623@gmail.com>
2018-02-28 10:30:58 -0800

    done

--- coloring-two/src/App.js
+++ coloring-two/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Star from './Assets/star.png'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <img className='Star3' alt='' src={Star}/>
+        <img className='Star4' alt='' src={Star}/>
+        <img className='Star' alt='' src={Star}/>
+        <img className='Star2' alt='' src={Star}/>        </div>
+      <div className='up'>
+      <div>
+        <div className='Vote'>Vote</div>
+        </div>
+        <div>
+        <div className='Judge'>Judge</div>
+        </div>
+        <div>
+        <div className='Woods'>Woods</div>
+        </div>
+        </div>
       </div>
     );
   }
